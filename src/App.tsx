82f18656d690dbe80c3a079b82dd3c24fc2831cc
spleekz <Shutnik-12K@yandex,ru>
2021-11-04 21:40:41 +0300

    add global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
 import React, { FC } from 'react'
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyles = createGlobalStyle`
+  * {
+    margin:0;
+    padding:0;
+  }
+`
 
 export const App: FC = () => {
-  return <div>phrase</div>
+  return (
+    <>
+      <GlobalStyles />
+      <div>phrase</div>
+    </>
+  )
 }
