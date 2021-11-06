d705b9ef1e86877f342e03e2d0fc0391cc395055
spleekz <Shutnik-12K@yandex,ru>
2021-11-06 20:01:21 +0300

    render copy button

--- src/components/Phrases.tsx
+++ src/components/Phrases.tsx
@@ -2,8 +2,11 @@ import { observer } from 'mobx-react-lite'
 import React, { FC } from 'react'
 import styled from 'styled-components'
 import { useStore } from '../stores/RootStore/RootStoreContext'
+import { CopyButton } from './CopyButton'
 
 const PhrasesBlock = styled.div`
+  display: flex;
+  justify-content: space-between;
   width: 710px;
   margin: 15px 0;
   padding: 10px;
@@ -11,20 +14,29 @@ const PhrasesBlock = styled.div`
   box-shadow: 0 5px 15px rgb(0 0 0 / 8%);
   border-radius: 4px;
 `
+const PhraseList = styled.div`
+  margin-right: 10px;
+`
 const PhraseBlock = styled.div`
   font-size: 24px;
-  color: #333333;
   background-color: #ffffff;
+  color: #333333;
 `
+const ButtonContainer = styled.div``
 
 export const Phrases: FC = observer((): JSX.Element => {
   const { PhrasesStore } = useStore()
 
   return (
     <PhrasesBlock>
-      {PhrasesStore.customPhrase.map((phrase) => {
-        return <PhraseBlock key={phrase}>{phrase}</PhraseBlock>
-      })}
+      <PhraseList>
+        {PhrasesStore.customPhrase.map((phrase) => {
+          return <PhraseBlock key={phrase}>{phrase}</PhraseBlock>
+        })}
+      </PhraseList>
+      <ButtonContainer>
+        <CopyButton text={PhrasesStore.customPhraseString} />
+      </ButtonContainer>
     </PhrasesBlock>
   )
 })
